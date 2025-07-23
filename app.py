bb0072c8a37461236edf3bf0c1a069419145ba93
Sanjanaaa <161009429+SanjanaVichare@users.noreply.github.com>
2025-07-24 02:06:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,4 +85,6 @@ def get_car_logs():
     return jsonify(logs)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render provides PORT env variable
+    app.run(host='0.0.0.0', port=port, debug=True)
+
